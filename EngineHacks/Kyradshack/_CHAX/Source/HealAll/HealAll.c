a38f67d85a987f521d9b0632ae50cb8a3cc5323a
Kyrads <nicholas7xyz@gmail.com>
2025-11-14 22:08:55 +0000

    Hui Augury

---
+++ EngineHacks/Kyradshack/_CHAX/Source/HealAll/HealAll.c
@@ -0,0 +1,18 @@
+//Menu Stuff
+//Heal All
+
+//Heal Individual
+
+//After starting fight have a popup 'do you want to heal your units?' if any units below max
+
+//Functionality
+//Heal All
+//Find units who need healing, for loop or something? Store all their missing HP
+//Calculate all missing HP, multiply for gold value
+//When button pressed check gold
+//if gold is too low play buzzer and dont do anything
+//if gold enough play a chaching sound and then a heal noise, stop cursor temporarily
+//For loop(?) again to find each unit missing hp and set it to max
+//Take the money
+
+//Heal Individual
