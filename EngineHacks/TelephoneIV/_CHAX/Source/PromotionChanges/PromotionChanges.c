f61712b5682fcd1d5ca26563a258aa7c8f1e0872
Pikmin1211 <joefarr1211@yahoo.ca>
2026-02-06 19:22:20 -0500

    fix thing

--- EngineHacks/TelephoneIV/_CHAX/Source/PromotionChanges/PromotionChanges.c
+++ EngineHacks/TelephoneIV/_CHAX/Source/PromotionChanges/PromotionChanges.c
@@ -29,156 +29,178 @@ extern const struct PromotionBonuses UnitPromotionBonusTable[];
 
 const struct PromotionBonuses* GetUnitPromotionBonuses(struct Unit* unit)
 {
-    for (int i = 0; UnitPromotionBonusTable[i].charID != 0; i++)
-    {
-        if (UnitPromotionBonusTable[i].charID == unit->pCharacterData->number) { return &UnitPromotionBonusTable[i]; }
+    int i = 0;
+    while(1){
+        if (UnitPromotionBonusTable[i].charID == 0){
+            return 0;
+        }
+        if (UnitPromotionBonusTable[i].charID == unit->pCharacterData->number) 
+            { return &UnitPromotionBonusTable[i]; }
+        i++;
     }
 
-    return NULL;
+    return 0;
 }
 
 void ApplyUnitPromotionSpecific(struct Unit* unit, const struct ClassData* promotedClass, const struct PromotionBonuses* bonuses)
 {
     unit->maxHP += bonuses->hpBonus;
-    if (unit->maxHP > promotedClass->maxHP) { unit->maxHP = promotedClass->maxHP; }
+    if (unit->maxHP > promotedClass->maxHP) 
+        { unit->maxHP = promotedClass->maxHP; }
 
     unit->pow += bonuses->strBonus;
-    if (unit->pow > promotedClass->maxPow) { unit->pow = promotedClass->maxPow; }
+    if (unit->pow > promotedClass->maxPow) 
+        { unit->pow = promotedClass->maxPow; }
 
     unit->mag += bonuses->magBonus;
-    if (unit->mag > MagClassTable[promotedClass->number].maxMag) {unit->mag = MagClassTable[promotedClass->number].maxMag; }
+    if (unit->mag > MagClassTable[promotedClass->number].maxMag) 
+        {unit->mag = MagClassTable[promotedClass->number].maxMag; }
 
     unit->skl += bonuses->sklBonus;
-    if (unit->skl > promotedClass->maxSkl) { unit->skl = promotedClass->maxSkl; }
+    if (unit->skl > promotedClass->maxSkl) 
+        { unit->skl = promotedClass->maxSkl; }
 
     unit->spd += bonuses->spdBonus;
-    if (unit->spd > promotedClass->maxSpd) { unit->spd = promotedClass->maxSpd; }
+    if (unit->spd > promotedClass->maxSpd) 
+        { unit->spd = promotedClass->maxSpd; }
 
     unit->def += bonuses->defBonus;
-    if (unit->def > promotedClass->maxDef) { unit->def = promotedClass->maxDef; }
+    if (unit->def > promotedClass->maxDef) 
+        { unit->def = promotedClass->maxDef; }
 
     unit->res += bonuses->resBonus;
-    if (unit->res > promotedClass->maxRes) { unit->res = promotedClass->maxRes; }
+    if (unit->res > promotedClass->maxRes) 
+        { unit->res = promotedClass->maxRes; }
 
     unit->lck += bonuses->lckBonus;
-    if (unit->lck > 30) { unit->lck = 30; }
+    if (unit->lck > 30) 
+        { unit->lck = 30; }
 
     //unit->conBonus += bonuses->conBonus; 
     // uh idk how you are capping con in your project so add that here
 
     unit->ranks[ITYPE_SWORD] += bonuses->swdWexp;
-    if (unit->ranks[ITYPE_SWORD] > WPN_EXP_S) { unit->ranks[ITYPE_SWORD] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_SWORD] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_SWORD] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_LANCE] += bonuses->lncWexp;
-    if (unit->ranks[ITYPE_LANCE] > WPN_EXP_S) { unit->ranks[ITYPE_LANCE] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_LANCE] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_LANCE] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_AXE] += bonuses->axeWexp;
-    if (unit->ranks[ITYPE_AXE] > WPN_EXP_S) { unit->ranks[ITYPE_AXE] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_AXE] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_AXE] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_BOW] += bonuses->swdWexp;
-    if (unit->ranks[ITYPE_BOW] > WPN_EXP_S) { unit->ranks[ITYPE_BOW] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_BOW] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_BOW] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_ANIMA] += bonuses->aniWexp;
-    if (unit->ranks[ITYPE_ANIMA] > WPN_EXP_S) { unit->ranks[ITYPE_ANIMA] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_ANIMA] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_ANIMA] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_LIGHT] += bonuses->lgtWexp;
-    if (unit->ranks[ITYPE_LIGHT] > WPN_EXP_S) { unit->ranks[ITYPE_LIGHT] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_LIGHT] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_LIGHT] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_DARK] += bonuses->drkWexp;
-    if (unit->ranks[ITYPE_DARK] > WPN_EXP_S) { unit->ranks[ITYPE_DARK] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_DARK] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_DARK] = WPN_EXP_S; }
 
     unit->ranks[ITYPE_STAFF] += bonuses->stfWexp;
-    if (unit->ranks[ITYPE_STAFF] > WPN_EXP_S) { unit->ranks[ITYPE_STAFF] = WPN_EXP_S; }
+    if (unit->ranks[ITYPE_STAFF] > WPN_EXP_S) 
+        { unit->ranks[ITYPE_STAFF] = WPN_EXP_S; }
 
     unit->pClassData = promotedClass;
 
     unit->level = 1;
     unit->exp = 0;
 
-    unit->curHP += bonuses->hpBonus;
-    if (unit->curHP > unit->maxHP) { unit->curHP = unit->maxHP; }
+    /*unit->curHP += bonuses->hpBonus;
+    if (unit->curHP > unit->maxHP) 
+        { unit->curHP = unit->maxHP; }
+        */
 
 }
 
 void ApplyUnitPromotion(struct Unit* unit, u8 classId) {
+
     const struct ClassData* promotedClass = GetClassData(classId);
-
-    int baseClassId = unit->pClassData->number;
-    int promClassId = promotedClass->number;
-
-    int i;
-
     const struct PromotionBonuses* bonuses = GetUnitPromotionBonuses(unit);
-    if (bonuses) { 
+
+    if (bonuses != 0) { 
         ApplyUnitPromotionSpecific(unit, promotedClass, bonuses); 
-        return; 
+    } else {
+        // Apply stat ups
+
+        unit->maxHP += promotedClass->promotionHp;
+
+        if (unit->maxHP > promotedClass->maxHP){
+            unit->maxHP = promotedClass->maxHP;
+        }
+
+        unit->pow += promotedClass->promotionPow;
+
+        if (unit->pow > promotedClass->maxPow){
+            unit->pow = promotedClass->maxPow;
+        }
+
+        unit->mag += MagClassTable[promotedClass->number].promotionMag;
+
+        if (unit->mag > MagClassTable[promotedClass->number].maxMag){
+            unit->mag = MagClassTable[promotedClass->number].maxMag;
+        }
+
+        unit->skl += promotedClass->promotionSkl;
+
+        if (unit->skl > promotedClass->maxSkl){
+            unit->skl = promotedClass->maxSkl;
+        }
+
+        unit->spd += promotedClass->promotionSpd;
+
+        if (unit->spd > promotedClass->maxSpd){
+            unit->spd = promotedClass->maxSpd;
+        }
+
+        unit->def += promotedClass->promotionDef;
+
+        if (unit->def > promotedClass->maxDef)
+            unit->def = promotedClass->maxDef;
+
+        unit->res += promotedClass->promotionRes;
+
+        if (unit->res > promotedClass->maxRes){
+            unit->res = promotedClass->maxRes;
+        }
+
+        // Remove base class' base wexp from unit wexp
+        for (int i = 0; i < 8; ++i){
+            unit->ranks[i] -= unit->pClassData->baseRanks[i];
+        }
+
+        // Update unit class
+        unit->pClassData = promotedClass;
+
+        // Add promoted class' base wexp to unit wexp
+        for (int i = 0; i < 8; ++i) {
+            int wexp = unit->ranks[i];
+
+            wexp += unit->pClassData->baseRanks[i];
+
+            if (wexp > WPN_EXP_S)
+                wexp = WPN_EXP_S;
+
+            unit->ranks[i] = wexp;
+        }
+
+        unit->level = 1;
+        unit->exp   = 0;
+
+        //unit->curHP += promotedClass->promotionHp;
+
+        if (unit->curHP > GetUnitMaxHp(unit)){
+            unit->curHP = GetUnitMaxHp(unit);
+        }
     }
-
-    // Apply stat ups
-
-    unit->maxHP += promotedClass->promotionHp;
-
-    if (unit->maxHP > promotedClass->maxHP)
-        unit->maxHP = promotedClass->maxHP;
-
-    unit->pow += promotedClass->promotionPow;
-
-    if (unit->pow > promotedClass->maxPow)
-        unit->pow = promotedClass->maxPow;
-
-    unit->mag += MagClassTable[promotedClass->number].promotionMag;
-
-    if (unit->mag > MagClassTable[promotedClass->number].maxMag)
-        unit->mag = MagClassTable[promotedClass->number].maxMag;
-
-    unit->skl += promotedClass->promotionSkl;
-
-    if (unit->skl > promotedClass->maxSkl)
-        unit->skl = promotedClass->maxSkl;
-
-    unit->spd += promotedClass->promotionSpd;
-
-    if (unit->spd > promotedClass->maxSpd)
-        unit->spd = promotedClass->maxSpd;
-
-    unit->def += promotedClass->promotionDef;
-
-    if (unit->def > promotedClass->maxDef)
-        unit->def = promotedClass->maxDef;
-
-    unit->res += promotedClass->promotionRes;
-
-    if (unit->res > promotedClass->maxRes)
-        unit->res = promotedClass->maxRes;
-
-    // Remove base class' base wexp from unit wexp
-    for (i = 0; i < 8; ++i)
-        unit->ranks[i] -= unit->pClassData->baseRanks[i];
-
-    // Update unit class
-    unit->pClassData = promotedClass;
-
-    // Add promoted class' base wexp to unit wexp
-    for (i = 0; i < 8; ++i) {
-        int wexp = unit->ranks[i];
-
-        wexp += unit->pClassData->baseRanks[i];
-
-        if (wexp > WPN_EXP_S)
-            wexp = WPN_EXP_S;
-
-        unit->ranks[i] = wexp;
-    }
-
-    // If Pupil -> Shaman promotion, set Anima rank to 0
-    if (baseClassId == CLASS_PUPIL && promClassId == CLASS_SHAMAN)
-        unit->ranks[ITYPE_ANIMA] = 0;
-
-    unit->level = 1;
-    unit->exp   = 0;
-
-    unit->curHP += promotedClass->promotionHp;
-
-    if (unit->curHP > GetUnitMaxHp(unit))
-        unit->curHP = GetUnitMaxHp(unit);
 }
